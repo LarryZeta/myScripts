49d05b973770be624166d47786dfbde2d1eef4c8
Larry Zeta <i@larryzeta.cc>
2020-02-04 15:03:26 +0800

    Update Get-Docker-CE-for-Debian.sh

--- Get-Docker-CE-for-Debian.sh
+++ Get-Docker-CE-for-Debian.sh
@@ -40,8 +40,7 @@ sudo apt-get update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 
 # INSTALL DOCKER COMPOSE
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo apt install -y docker-compose
 
 else
 echo "Abort."
